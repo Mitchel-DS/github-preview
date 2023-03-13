2edcfe979ba8c0f88a2c2eec78684fe5910b1ef0
Mitchel-DS <mitchel.staal@outlook.com>
2023-03-13 14:22:15 +0100

    installed sass

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import './style.css'
 
+try {
+  const a = 1 + 2
+  console.log(a)
+} catch (e) {
+  console.log(e.status)
+}
+
+console.log('Hello World');
+
 document.querySelector('#app').innerHTML = `
   <h1>Hello.</h1>
 `
